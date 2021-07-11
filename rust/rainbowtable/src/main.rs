f4fc6a7dde5a47cffdd4bb656d416f669416dd17
Marvin Davieds <marvin.davieds@gmail.com>
2021-07-11 12:08:03 +0200

    rust: add lookup

--- rust/rainbowtable/src/main.rs
+++ rust/rainbowtable/src/main.rs
@@ -22,7 +22,7 @@ const RAINBOW_TABLE_PATH: &str = "./rainbow.table";
 // Length of reduced passwords and hashes in characters (i.e. hash length is LENGTH*4 bits).
 const LENGTH: usize = 8;
 // Number of iterations for a rainbow table chain.
-const ITERATIONS: u32 = 100;
+const ITERATIONS: u32 = 10000;
 
 const HASH_FUNC: HashFunc = |str| {
     let result = Sha3_224::digest(&str.as_bytes());
@@ -32,8 +32,8 @@ const HASH_FUNC: HashFunc = |str| {
 const REDUCTION_FUNC: ReductionFunc =
     |hash, index| reduction_function1(hash, LENGTH as u32, index);
 
-fn _main_save() {
-    let wordlist = match read_wordlist(RAINBOW_TABLE_PATH) {
+fn _main() {
+    let wordlist = match read_wordlist("./wordlist.txt") {
         Ok(wordlist) => wordlist,
         Err(err) => panic!("{}", err),
     };
@@ -51,7 +51,7 @@ fn _main_save() {
         elapsed,
     );
 
-    if let Err(err) = rt.save_to_file("./rainbow.table") {
+    if let Err(err) = rt.save_to_file(RAINBOW_TABLE_PATH) {
         println!("Failed to save rainbow table to file: {}", err);
         process::exit(1);
     }
@@ -65,4 +65,18 @@ fn main() {
     }
 
     println!("Loaded {} chains into rainbow table", rt.get_chain_count());
+
+    // Do a lookup of a hashed plaintext from the first column.
+    let plaintext = "aardvark";
+    let hash = (HASH_FUNC)(plaintext);
+
+    let start = Instant::now();
+    let result = rt.lookup(&hash);
+    let elapsed = start.elapsed().as_secs_f64();
+
+    match result {
+        Ok(Some(candidate)) => println!("Found plaintext {} for hash {} in {}s", &candidate, &hash, elapsed),
+        Ok(None) => println!("Found no plaintext for hash {} in {}s", &hash, elapsed),
+        Err(err) => println!("Lookup failed: {}", err)
+    }
 }
--- rust/rainbowtable/src/rainbow_table.rs
+++ rust/rainbowtable/src/rainbow_table.rs
@@ -1,8 +1,21 @@
 pub mod reduction;
 
+use custom_error::custom_error;
 use rayon::prelude::*;
 use reduction::ReductionError;
 use std::collections::HashMap;
+
+custom_error! {pub LookupError
+    ReductionError{source: ReductionError} = "{source}",
+    InvalidMatch{
+        hash: String,
+        iteration: u32,
+        first_column: String,
+        last_column: String,
+        plaintext: String
+    } = "found matching hash {} at iteration {} in chain ({}, {}) but validation of plaintext {plaintext} failed",
+    Other{message: String} = "{message}",
+}
 
 pub type HashFunc = fn(&str) -> String;
 
@@ -62,17 +75,18 @@ impl RainbowTable {
 
     /// Loads a rainbow table from a CSV file into this instance.
     pub fn load_from_file(&mut self, path: &str) -> Result<(), csv::Error> {
-      let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(false)
-        .from_path(path)?;
+        let mut rdr = csv::ReaderBuilder::new()
+            .has_headers(false)
+            .from_path(path)?;
 
-      self.table.clear();
-      for result in rdr.records() {
-          let record = result?;
-          self.table.insert(String::from(&record[1]), String::from(&record[0]));
-      }
+        self.table.clear();
+        for result in rdr.records() {
+            let record = result?;
+            self.table
+                .insert(String::from(&record[1]), String::from(&record[0]));
+        }
 
-      Ok(())
+        Ok(())
     }
 
     /// Saves the current rainbow table as a CSV file at the given path.
@@ -86,6 +100,65 @@ impl RainbowTable {
         Ok(())
     }
 
+    /// Searches the rainbow table for a plaintext matching the given hash value.
+    pub fn lookup(&self, hash_str: &str) -> Result<Option<String>, LookupError> {
+        for i in (1..self.iterations).rev() {
+            match self.lookup_for_iteration(hash_str, i) {
+                Ok(Some(candidate)) => return Ok(Some(candidate)),
+                Ok(None) => continue,
+                Err(_) => {
+                    // println!("Lookup error: {}. Continuing ...", err);
+                    continue;
+                }
+            }
+        }
+
+        Ok(None)
+    }
+
+    /// Checks whether the given hash is at the column of the rainbow table specified by the given
+    /// iteration.
+    fn lookup_for_iteration(
+        &self,
+        hash_str: &str,
+        iteration: u32,
+    ) -> Result<Option<String>, LookupError> {
+        let mut tmp_hash_str = hash_str.to_string();
+        let mut tmp_reduced_str = String::new();
+
+        // Calculate the chain, starting from iteration, going to last column at self.iterations-1.
+        for i in iteration..self.iterations {
+            tmp_reduced_str = (self.reduction_func)(&tmp_hash_str, i)?;
+            tmp_hash_str = (self.hash_func)(&tmp_reduced_str);
+        }
+
+        // Last column is in tmp_reduced_str. Look whether we find a matching first column.
+        match self.table.get(&tmp_reduced_str) {
+            Some(first_column) => {
+                // Now that we have found a matching first column, calculate the chain up to the
+                // column were our hash in question is.
+                let mut plaintext = first_column.clone();
+                for j in 1..iteration {
+                    plaintext = (self.reduction_func)(&(self.hash_func)(&plaintext), j)?;
+                }
+
+                // For good measure verify the match and return.
+                if (self.hash_func)(&plaintext) == hash_str {
+                    Ok(Some(plaintext))
+                } else {
+                    Err(LookupError::InvalidMatch {
+                        hash: hash_str.into(),
+                        iteration: iteration,
+                        first_column: first_column.into(),
+                        last_column: tmp_reduced_str.into(),
+                        plaintext: plaintext.into(),
+                    })
+                }
+            }
+            None => Ok(None),
+        }
+    }
+
     pub fn get_chain_count(&self) -> usize {
         return self.table.len();
     }
@@ -94,7 +167,7 @@ impl RainbowTable {
     /// column.
     fn calc_chain(&self, word: &str) -> Result<String, ReductionError> {
         let mut last_column = word.to_string();
-        for i in 1..(self.iterations) {
+        for i in 1..self.iterations {
             let hash = (self.hash_func)(&last_column);
             last_column = match (self.reduction_func)(&hash, i) {
                 Ok(next_plaintext) => next_plaintext,
