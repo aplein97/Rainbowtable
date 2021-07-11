0e00fb1d9352a4684cb1908b3f96e891145db662
Marvin Davieds <marvin.davieds@gmail.com>
2021-07-11 23:33:29 +0200

    add final touches

---
+++ python/rainbowtable/reduction.py
@@ -0,0 +1,21 @@
+
+def reduction_function1(hash_str: str, length: int, index: int) -> str:
+    """
+    A function to reduce a given hash to a plaintext password of the given length.
+
+    Based on https://link.springer.com/chapter/10.1007/978-3-642-30436-1_42
+    """
+    if length < 1:
+        raise ValueError('length must be greater 0')
+    if index < 0:
+        raise ValueError('index must be greater or equal 0')
+
+    # Convert hash from string into int
+    number = int(''.join(map(str, map(ord, hash_str))))
+    number = (number + index) % 26 ** length
+    result = ''
+    for _ in range(0, length):
+        result += chr((number % 26) + ord('a'))
+        number = number // 26
+
+    return result
